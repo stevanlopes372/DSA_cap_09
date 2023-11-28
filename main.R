eb69ffc640e55c9fc115589bd59e2bd0acc54182
Stevan Lopes <neo2geon@gmail.com>
2023-11-27 22:05:16 -0300

    Primeiro commit

---
+++ main.R
@@ -0,0 +1,13 @@
+setwd("C:/Users/neo2g/OneDrive/Documentos/DataScience Academy/Big Data R Azure/Cap 09/DSA_cap_09")
+getwd()
+
+
+# Distribuição Poisson
+?dpois
+
+## Exemplo: considere um processo que tem uma taxa de 0,5 defeitos por unidade
+## Qual a probabilidade de uma unidade apresentar dois defeitos?
+dpois(2, 0.5)
+
+## E nenhum defeito?
+dpois(0, 0.5)
