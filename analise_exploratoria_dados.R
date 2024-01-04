4f9661ca918a3dae3f4bfa971e8888214fc9a702
Stevan Lopes <neo2geon@gmail.com>
2024-01-03 23:01:36 -0300

    finalizada análise exploratória de dados

--- analise_exploratoria_dados.R
+++ analise_exploratoria_dados.R
@@ -37,15 +37,68 @@ quantile(df$Sepal.Length)
 
 # Boxplot
 # Leitura de Baixo para Cima - Q1, Q2 (Mediana) e Q3
+boxplot(df)
 
 # Histograma
 # Indicam a frequência de valores dentro de cada bin (classe de valores)
+hist(df$Sepal.Length)
+hist(df$Sepal.Width)
+hist(df$Petal.Width)
+
+
 
 # Scatterplot Preço x Km
 # Usando o preço como variável dependente (y)
+plot(
+  df$Sepal.Length
+  ,df$Sepal.Width
+  ,main = "Scatterplot"
+  )
+
+plot(
+  df$Sepal.Length
+  ,df$Petal.Length
+  ,main = "Scatterplot"
+  )
+
+plot(
+  df$Sepal.Length
+  ,df$Petal.Width
+  ,main = "Scatterplot"
+)
+
+plot(
+  df$Petal.Length
+  ,df$Petal.Width
+  ,main="Scatterplot"
+)
+
 
 # Medidas de Dispersão
 # Ao interpretar a variância, números maiores indicam que 
 # os dados estão espalhados mais amplamente em torno da 
 # média. O desvio padrão indica, em média, a quantidade 
 # de cada valor diferente da média.
+var(df$Sepal.Length)
+sd(df$Sepal.Length)
+
+
+
+##### Análise Exploratória de Dados Para Variáveis Categóricas ##### 
+
+# Criando tabelas de contingência - representam uma única variável categórica
+# Lista as categorias das variáveis nominais
+?table
+str(df)
+table(df$Species)
+
+
+# Calculando a proporção de cada categoria
+model_table <- table(df$Species)
+prop.table(model_table)
+
+# Arrendondando os valores
+model_table <- table(df$Species)
+model_table <- prop.table(model_table) * 100
+round(model_table, digits = 0)
+
